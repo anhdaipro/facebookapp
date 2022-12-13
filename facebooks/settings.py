7cc4ab2cdbe9c9e1a8f940801b8254b05e823a3a
phamdai <daipham952@gmail.com>
2022-12-13 11:04:41 +0700

    first-commit

--- facebooks/settings.py
+++ facebooks/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000','https://facebook-123.vercel.app'
+    'http://localhost:3000','https://facebook-rose.vercel.app'
 ]
 # Application definition
 CORS_ORIGIN_ALLOW_ALL = False
